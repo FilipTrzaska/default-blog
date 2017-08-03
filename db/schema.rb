0ad2c1e770a701f20b3865df3ba52ea4a571a8bc
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-08-03 09:19:01 +0000

    creating articles added

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170801185255) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "text"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updates_at"
+  end
 
 end
