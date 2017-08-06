bc0a4b15e3f50ae571e505e15d657c16664deaaa
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-08-06 07:04:26 +0000

    next

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806052105) do
+ActiveRecord::Schema.define(version: 20170806054346) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "text"
