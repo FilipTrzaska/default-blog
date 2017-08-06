2402917239a542783fbe4013aebc64a5b61bd5d5
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-08-06 10:24:47 +0000

    show action and gravatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806070904) do
+ActiveRecord::Schema.define(version: 20170806101933) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "text"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updates_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
