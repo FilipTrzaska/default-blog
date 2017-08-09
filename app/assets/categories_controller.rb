b5a63778071498774dc721214611181dcc5357b1
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-08-09 06:50:33 +0000

    added test do categories

--- app/assets/categories_controller.rb
+++ app/assets/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
     def index
-        
+        @categories = Category.all
     end
     
     def new
-        
+        @category = Category.new
     end
     
     def show
@@ -12,6 +12,18 @@ class CategoriesController < ApplicationController
     end
     
     def create
-        
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category created successfully"
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def category_params
+       params.require(:category).permit(:name) 
     end
 end
